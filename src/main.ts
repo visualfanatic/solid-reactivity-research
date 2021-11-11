5d7e58060896720fc07be759a9f948dd5137cb9d
Damian Stasik <visualfanatic@users.noreply.github.com>
2021-11-11 15:45:52 +0100

    Start using the reactivity api

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,27 @@
+import {
+  createRoot,
+  createSignal,
+  createEffect
+} from './signal';
+
 const app = document.querySelector<HTMLDivElement>('#app')!
 
-app.innerHTML = `<h1>Hello Vite!</h1>`
+createRoot(() => {
+  const [count, setCount] = createSignal(0);
+
+  const btn = document.createElement('button');
+  btn.innerHTML = '+1';
+  btn.addEventListener('click', () => setCount((count) => count + 1));
+
+  const text = document.createElement('span');
+
+  createEffect(() => {
+    text.innerHTML = String(count());
+  });
+
+  app.appendChild(btn);
+  app.appendChild(text);
+});
+
+
+
